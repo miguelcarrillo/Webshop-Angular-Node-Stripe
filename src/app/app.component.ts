417044ee7a38a1d88b2b0668014e1d9c176045f5
Miguel Carrillo <miguelcarrillo125@gmail.com>
2023-01-17 12:58:45 -0500

    Creates app header and Sets up hot loading for tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: ` <h1 class="text-3xl font-bold underline">Hello world!</h1> `,
+  template: `<app-header></app-header>`,
   styles: [],
 })
 export class AppComponent {
