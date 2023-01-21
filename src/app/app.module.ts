6f40aefd38c2259ca389469f8e98ecb3f95c4d6b
Miguel Carrillo <miguelcarrillo125@gmail.com>
2023-01-21 08:18:35 -0500

    Create homepage component with products header and product filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,17 @@ import { MatTableModule } from '@angular/material/table';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 2: 335, 4: 350 };
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.component.html",
+})
+export class HomeComponent implements OnInit {
+  category: string | undefined;
+  cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+}
